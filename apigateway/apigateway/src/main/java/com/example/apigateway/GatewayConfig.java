906a99e115567aacc78843c29fa5704ee0aec95d
Raunak <rounand44@gmail.com>
2026-04-19 23:34:11 +0530

    added gateway config

---
+++ apigateway/apigateway/src/main/java/com/example/apigateway/GatewayConfig.java
@@ -0,0 +1,16 @@
+package com.example.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        return routeLocatorBuilder.routes();
+    }
+}
