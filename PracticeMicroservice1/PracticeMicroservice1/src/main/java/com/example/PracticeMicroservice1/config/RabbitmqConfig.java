79e144262e2e7c79af1edd4372af6b36f7262b87
Raunak <rounand44@gmail.com>
2026-04-24 23:52:55 +0530

    added config file of rabbitmq

--- PracticeMicroservice1/PracticeMicroservice1/src/main/java/com/example/PracticeMicroservice1/config/RabbitmqConfig.java
+++ PracticeMicroservice1/PracticeMicroservice1/src/main/java/com/example/PracticeMicroservice1/config/RabbitmqConfig.java
@@ -1,6 +1,8 @@
 package com.example.PracticeMicroservice1.config;
 
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,4 +16,30 @@ public class RabbitmqConfig {
 
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
+
+    @Bean
+    public Queue queue()
+    {
+        return QueueBuilder.durable(queueName).build();
+    }
+
+    @Bean
+    public TopicExchange topicExchange()
+    {
+        return ExchangeBuilder
+                .topicExchange(exchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding binding()
+    {
+        return BindingBuilder
+                .bind(queue())
+                .to(topicExchange())
+                .with(routingKey);
+    }
+
+
 }
